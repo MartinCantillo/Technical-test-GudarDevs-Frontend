e5b713ea81708feaecf41ca49c19a4cdb1b69377
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-04-12 15:06:16 -0500

    fixing

--- Project/src/app/Components/chome/chome.component.ts
+++ Project/src/app/Components/chome/chome.component.ts
@@ -1,32 +1,34 @@
 import { Component } from '@angular/core';
-import { AdditionalField } from 'src/app/Models/AdditionalField';
-import { Contact } from 'src/app/Models/Contact';
-import { ContactType } from 'src/app/Models/ContactType';
-import { SContactService } from 'src/app/Services/scontact.service';
 
+import { Contact } from 'src/app/Models/Contact';
+import { SContactService } from 'src/app/Services/scontact.service';
 @Component({
   selector: 'app-chome',
   templateUrl: './chome.component.html',
   styleUrls: ['./chome.component.css']
 })
 export class CHomeComponent {
-  //Models
-  additionalField: AdditionalField = new AdditionalField();
+
   contact: Contact = new Contact();
-  contactType: ContactType = new ContactType();
-  //list of contacts
   contacts: Contact[] = [];
+
+
+
   constructor(private _SContactService: SContactService) {
-    this.LoadContacts();
+    this.loadContacts();
   }
 
-  private LoadContacts() {
+  ngOnInit() {
+
+  }
+
+  private loadContacts() {
     this._SContactService.GetAll().subscribe(
-      //Arrow function, funcion anónima similar a expersiones Lambda , aqui relleno el vector
-      (Data) => {
-        this.contacts = Data;
+      (data) => {
+        this.contacts = data;
       }
     );
   }
+
 
 }
--- Project/src/app/app-routing.module.ts
+++ Project/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CHomeComponent } from './Components/chome/chome.component';
 import { CloginComponentComponent } from './Components/clogin-component/clogin-component.component';
 
-const routes: Routes = [ { path: "loggin", component: CloginComponentComponent },
+const routes: Routes = [{ path: "loggin", component: CloginComponentComponent },
 { path: "Home", component: CHomeComponent }];
 
 @NgModule({
