bc6b0c8bfdd5b0597c9d9fe9157cb8a53ea86895
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-04-13 16:24:16 -0500

    guard was added

--- Project/src/app/Guard/contact-guard.guard.ts
+++ Project/src/app/Guard/contact-guard.guard.ts
@@ -1,22 +1,23 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { AuthServiceService } from '../Services/auth-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactGuardGuard implements CanActivate {
-  constructor(private auth:AuthServiceService , private router: Router){
+  constructor(private auth: AuthServiceService, private router: Router) { }
 
-  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if (!this.auth.isAuthenticated()) {
-        return this.router.navigate(['/login']).then(() => false);
-      }
-      return true;
+      return this.auth.isAuthenticated().pipe(map((isAuthenticated: boolean) => {
+        if (!isAuthenticated) {
+          return this.router.parseUrl('/loggin');
+        }
+        return true;
+      }));
   }
-
 }
